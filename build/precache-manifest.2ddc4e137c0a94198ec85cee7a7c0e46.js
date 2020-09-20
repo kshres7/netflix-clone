dcde1c901c3f56c3b32f55d62c3b475200eb48be
Karan Shrestha <karanshrestha96@gmail.com>
2020-09-20 00:26:42 -0400

    Deployed the web app on firebase

---
+++ build/precache-manifest.2ddc4e137c0a94198ec85cee7a7c0e46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8f89838632d25ce6134fd28b2dfccbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7dd1726d3d88b91e4fdf",
+    "url": "/static/css/main.8884d7f7.chunk.css"
+  },
+  {
+    "revision": "5e01fc737a25445ad27c",
+    "url": "/static/js/2.d45d450a.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.d45d450a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dd1726d3d88b91e4fdf",
+    "url": "/static/js/main.1c7c8767.chunk.js"
+  },
+  {
+    "revision": "07b33697149324db54e5",
+    "url": "/static/js/runtime-main.b8a87127.js"
+  }
+]);
